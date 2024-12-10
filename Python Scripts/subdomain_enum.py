f0ea607df177eac0e791fe53be2f3df167c07d40
Lorenzo Solito <105752201+M2F14M3@users.noreply.github.com>
2024-12-10 17:52:18 +0100

    Update subdomain_enum.py

--- Python Scripts/subdomain_enum.py
+++ Python Scripts/subdomain_enum.py
@@ -1,15 +1,23 @@
+# Libraries import.
 import requests
 import sys
 
-wordlist_file = "<path_file>"
+# Variables declaration.
+# Change from "path_file" to the actual path of the wordlist file.
+wordlist_file = "path_file"
+# Opening of the wordlist file in reading mode.
 sub_list = open(wordlist_file).read()
+# Assing all the content of the wordlist file to an array.
+# One cell per each line.
 subdoms = sub_list.splitlines()
 
 for sub in subdoms:
+	# Definition of the HTTP URL request.
 	sub_domains = f"http://{sub}.{sys.argv[1]}"
-
 	try:
+		# HTTP GET request for the defined URL.
 		requests.get(sub_domains)
+	# Check if the GET Request get back an error (the request URL is not found).
 	except requests.ConnectionError:
 		pass
 	else:
